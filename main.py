32870f04147a6e45d6b2439669488680d0a6522f
LeeBotting07 <148448610+LeeBotting07@users.noreply.github.com>
2024-12-02 15:01:28 +0000

    Refactor project structure by removing old templates and styles, and adding new main layout and CSS

--- main.py
+++ main.py
@@ -8,3 +8,10 @@ app = Flask(__name__)
 @app.route('/home')
 def home():
     return render_template('index.html', title="Home")
+
+@app.route('/weather')
+def weather():
+    return render_template('weather.html', title="Weather")
+
+if __name__ == '__main__':
+    app.run(debug=True)
